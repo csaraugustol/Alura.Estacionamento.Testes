20be35c7e09ad8172ab8a88fd530d689658f68d3
csaraugustol <cesar.leitejm@hotmail.com>
2022-06-22 09:55:39 -0300

    Primeiros testes

---
+++ Alura.Estacionamento.Testes/TipoVeiculoTestes.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class TipoVeiculoTestes
+    {
+        [Fact]
+        public void TestaTipoDeVeiculoEhAutomovel()
+        {
+            var veiculo = new Veiculo();
+
+            Assert.Equal(TipoVeiculo.Automovel, veiculo.Tipo);
+        }
+    }
+}
---
+++ Alura.Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using System;
+using Alura.Estacionamento.Modelos;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Acelerar(10);
+
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFreiar()
+        {
+            var veiculo = new Veiculo();
+            veiculo.Frear(10);
+
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
